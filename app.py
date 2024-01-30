b66fddb34abfd3cf47ae8ffeda89a5d75b44729c
HanDaeHee <roklp9@gmail.com>
2024-01-30 17:28:38 +0900

    샘플

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import streamlit as st
 import pandas as pd
 
-# Matplotlib의 전역 figure 경고 무시 설정
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 folder_path = "C:/Users/한대희/Desktop/mulcamp-240130/"
@@ -24,10 +23,12 @@ st.subheader("히스토그램")
 selected_column = st.selectbox("히스토그램", all_data.columns)
 fig, ax = plt.subplots(figsize=(8, 6))
 sns.histplot(all_data[selected_column].drop_duplicates(), kde=True, ax=ax)
-st.pyplot(fig)  # Matplotlib figure 객체를 전달
+st.pyplot(fig) 
+plt.clf()  
 
 st.subheader("박스 플롯")
 boxplot_column = st.selectbox("박스 플롯", all_data.columns)
 fig, ax = plt.subplots(figsize=(8, 6))
 sns.boxplot(x=all_data[boxplot_column], ax=ax)
-st.pyplot(fig)  # Matplotlib figure 객체를 전달
+st.pyplot(fig)  
+plt.clf()  
